92d81efab4f12439a400006319fbcb910b1ce3b1
Ramya V <ramyavenkatesan65@gmail.com>
2026-08-25 17:15:40 +0530

    Update app.py

--- AWS-Project/Student-app/app.py
+++ AWS-Project/Student-app/app.py
@@ -8,7 +8,7 @@ def get_db_connection():
         host="student-db.c582isu0m6qe.ap-south-1.rds.amazonaws.com",
         user="admin",
         password="YOUR-RDS-PASSWORD",
-        database="student-db"
+        database="studentdb"
     )
 
 @app.route("/")
