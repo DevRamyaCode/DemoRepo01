b40f2bc8f5f517e6fd373d40a5c612fcfa5446db
Ramya V <ramyavenkatesan65@gmail.com>
2026-09-05 19:49:34 +0530

    Handling duplication Backups-perform as Idempotent
    To overcome the problem of taking backup of the same snapshot whenever the code is executed , we have added the tags to the Backuped snapshot which will be checked before it performs backup part of the code.

--- AWS-Project/Backup Automation-DR/lambda_function.py
+++ AWS-Project/Backup Automation-DR/lambda_function.py
@@ -3,14 +3,11 @@ import boto3
 def lambda_handler(event, context):
     source_region = "ap-south-1"
     destination_region = "ap-south-2"
-    source_ec2 = boto3.client(
-        "ec2",
-        region_name=source_region
-    )
-    destination_ec2 = boto3.client(
-        "ec2",
-        region_name=destination_region
-    )
+
+    source_ec2 = boto3.client("ec2",region_name=source_region)
+
+    destination_ec2 = boto3.client("ec2",region_name=destination_region)
+
     response = source_ec2.describe_snapshots(
         OwnerIds=["self"],
         Filters=[
@@ -28,20 +25,41 @@ def lambda_handler(event, context):
             tag["Value"] == "true"
             for tag in tags
         )
-        if dr_enabled:
-            print(snap_id, "requires DR copy")
-            response = destination_ec2.copy_snapshot(
-                SourceRegion=source_region,
-                SourceSnapshotId=snap_id,
-                Description="Backup copy"
+        dr_copied = any(
+            tag["Key"] == "DR-Copied" and 
+            tag["Value"] == "true"
+            for tag in tags
+        )
+    if not dr_enabled:
+        print(snap_id, "does not require DR copy")
+
+    elif dr_copied:
+        print(snap_id, "already copied to Destination Region - Skipping")
+    else:
+        print(snap_id, "requires DR copy")
+
+        response = destination_ec2.copy_snapshot(
+            SourceRegion=source_region,
+            SourceSnapshotId=snap_id,
+            Description="Backup copy"
             )
-            print(
-                snap_id,
-                "copy started:",
-                response["SnapshotId"]
-            )
-        else:
-            print(
-                snap_id,
-                "does not require DR copy"
-            )
+
+        destination_snapshot_id = response["SnapshotId"]
+
+        print(snap_id,"copy started:", destination_snapshot_id)
+
+        source_ec2.create_tags(
+            Resources=[snap_id],
+            Tags=[
+                {
+                    "Key": "DR-Copied",
+                    "Value": "true"
+                    },
+               {
+                    "Key": "DR-Snapshot-ID",
+                    "Value": destination_snapshot_id
+                }
+            ]
+        )
+
+        print(snap_id,"is copied to destination Region")
